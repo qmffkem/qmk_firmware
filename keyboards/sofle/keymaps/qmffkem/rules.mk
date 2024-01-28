933e2673c99e5a48883c7d1615117af25605e733
qmffkem <wodndckato@gmail.com>
2024-01-28 00:58:31 -0500

    custom keymap

---
+++ keyboards/sofle/keymaps/qmffkem/rules.mk
@@ -0,0 +1,14 @@
+# keys
+EXTRAKEY_ENABLE = yes
+# VIA_ENABLE = yes
+
+# extras
+# RGBLIGHT_ENABLE = yes
+OLED_ENABLE = yes
+ENCODER_ENABLE = yes
+
+# optimization
+LTO_ENABLE = yes
+
+# debug
+CONSOLE_ENABLE = no
