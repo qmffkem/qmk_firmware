8c497bd6a095dc98c00392beb9a8efe9174de0ae
qmffkem <wodndckato@gmail.com>
2024-01-29 14:20:43 -0500

    Fix OLED rotation for non-master keyboards

--- keyboards/sofle/keymaps/qmffkem/keymap.c
+++ keyboards/sofle/keymaps/qmffkem/keymap.c
@@ -352,6 +352,8 @@ static void print_status_narrow(void) {
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     if (is_keyboard_master()) {
         return OLED_ROTATION_270;
+    } else {
+        return OLED_ROTATION_180;
     }
     return rotation;
 }
